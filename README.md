6b3bcabc4d48562186f37a383f0702c4462eb47d
Sayak Das <sayak.das@codeclouds.in>
2026-02-15 13:11:03 +0530

    jsx introdiuction code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # React Study
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- Parcel uses File Watching Algorithm written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differntial Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- Https
+- Tree Shaking - Removes unnecessary codes
--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const parent = React.createElement("div", 
-    {id : "parent"}, 
-    [   React.createElement("div", 
-        {id : "child"}, 
-        [React.createElement("h1", {id: "heading1"}, "I'm an h1 tag"),
-        React.createElement("h2", {id: "heading2"}, "I'm an h2 tag")
-        ]
-    ),
-   React.createElement("div", 
-        {id : "child2"}, 
-        [React.createElement("h1", {id: "heading1"}, "I'm an h1 tag"),
-        React.createElement("h2", {id: "heading2"}, "I'm an h2 tag")
-        ]
-    )]
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// const parent = React.createElement("div",
+//   { id: "parent" },
+//   [
+//     React.createElement("div",
+//       { id: "child", key: "child1" },
+//       [
+//         React.createElement("h1", { id: "heading1", key: "h1" }, "I'm an h1"),
+//         React.createElement("h2", { id: "heading2", key: "h2" }, "I'm an h2 tag")
+//       ]
+//     ),
 
-    );
+//     React.createElement("div",
+//       { id: "child2", key: "child2" },
+//       [
+//         React.createElement("h1", { id: "heading1", key: "h3" }, "I'm an h1 tag"),
+//         React.createElement("h2", { id: "heading2", key: "h4" }, "I'm an h2 tag")
+//       ]
+//     )
+//   ]
+// );
 
 
-const heading = React.createElement("h1", {id : "heading"}, "hello world from react!");
+// const heading = React.createElement("h1", {id : "heading"}, "hello world from react!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(root);
+// root.render(parent);
+
+const jsxHeading = <h1 id="heading">I am an h1 tag</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(parent);
+root.render(jsxHeading);
